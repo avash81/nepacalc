f58696242be0c3ac4295e7dd01c4efbb0515ca62
avash81 <avash.work.np@gmail.com>
2026-05-09 13:10:25 +0545

    feat: setup continuous internal link chaining and update seo content

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,22 +2,33 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: [
-        '/',
-        '/blog/',
-        '/favicon.ico',
-        '/favicon.png',
-        '/favicon-48x48.png',
-        '/logo.png'
-      ],
-      disallow: [
-        '/api/',
-        '/admin/',
-        '/search/',
-      ],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/admin/',
+          '/*?utm_',
+          '/*?ref=',
+          '/*?source='
+        ]
+      },
+      {
+        userAgent: 'bingbot',
+        allow: '/',
+        crawlDelay: 2
+      },
+      {
+        userAgent: 'YandexBot',
+        allow: '/',
+        crawlDelay: 5
+      },
+      {
+        userAgent: ['GPTBot', 'ClaudeBot', 'PerplexityBot', 'OAI-SearchBot', 'Google-Extended'],
+        allow: '/'
+      }
+    ],
     sitemap: 'https://nepacalc.com/sitemap.xml',
   };
 }
